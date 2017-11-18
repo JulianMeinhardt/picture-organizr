f6182a16992b5932d79c9247c3e9f6e62f804aae
Julian Meinhardt <julianmeinhardt@gmail.com>
2017-11-18 22:03:44 +0100

    adds on package json

--- po.folder.folderservice/index.js
+++ po.folder.folderservice/index.js
@@ -1,10 +1,11 @@
-const container = require('src/container');
+const container = require('./src/container');
 
 const app = container.resolve('app');
 
 app
   .start()
-  .catch((error) => {
+  .catch((err) => {
+    console.error('error while starting application', err);
     // TODO: Add logger if there was an exception while starting the app
     process.exit();
   });
