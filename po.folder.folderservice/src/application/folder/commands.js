f0617506b765505ddd4b1139fe59e76a3ee88bbb
Julian Meinhardt <julianmeinhardt@gmail.com>
2017-11-27 22:31:44 +0100

    create new folder from json object

--- po.folder.folderservice/src/application/folder/commands.js
+++ po.folder.folderservice/src/application/folder/commands.js
@@ -1,7 +1,13 @@
+const Folder = require('../../domain/folder');
+
 module.exports = {
   createFolder: async (body) => {
     console.log('this will create a folder');
     console.log('with this body', body);
+    const folder = new Folder(body.name, body.filepath);
+    // TODO: check if folder is valid
+    // TODO: save folder to database
+    return folder;
   },
   deleteFolderById: (id) => {
     console.log(`this will delete a folder with the id: ${id}`);
